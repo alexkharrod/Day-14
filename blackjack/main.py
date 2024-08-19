4488d6a84c7a881ce034c49db5da9deab77d18af
alexharrod <alexkharrod@gmail.com>
2024-08-19 18:55:13 -0400

    All finished with base version and everything seems to work.

--- blackjack/main.py
+++ blackjack/main.py
@@ -2,58 +2,48 @@ import random
 
 import art
 
-# TODO 1 - create 2 lists - 1 for you and 1 for dealer
-# TODO 2 - append your dealt cards with random
-# TODO 3 - show you your cards and your total.  show only 1 of the dealer cards.
-
-# TODO 4 - Ask if you want to hit or Stay and append cards and show accordingly ( slice dealers list 1:)
-
-# TODO 5 - Work out logic - dealer hits on anything < 17
-# TODO 6 - Ace value is 1 or 11 depending if you bust or are over ?? 17 ???
-
-# TODO 7 - Highest Score without busting wins
-#
-# TODO 8 - Announce winner and scores and your score if you lose.( YOU BUSTED, or your lose?)
-
-# TODO 9 - Add art and check everything
-
-# TODO 10 - Create a game condition that interrupts gameplay when you or the dealer bus, or neither takes a card.
 
 def main():
     """ This is the main game loop that runs the game. deals original hand, and checks for blackjack"""
     print(art.logo)
     print("Welcome to BLACKJACK!")
-    # game_over = False
-    # while game_over == False:
-    for deal in range(2):
-        #deal the cards
-        dealer_hand.append(random.choice(cards))
-        player_hand.append(random.choice(cards))
+    game_on = True
+    while game_on:
+        for deal in range(2):
+            #deal the cards
+            dealer_hand.append(random.choice(cards))
+            player_hand.append(random.choice(cards))
 
-    # if dealer has black jack dealer wins
-    if sum(dealer_hand) == 21 and len(dealer_hand) == 2:
-        print(f"Dealer WINS!  BlackJack!")
-        print(f"The Dealer's hand is : {dealer_hand}, which equals {sum(dealer_hand)}")
-        print(f"Your hand was {player_hand}, total score {sum(player_hand)}")
+        # if dealer has black jack dealer wins
+        if sum(dealer_hand) == 21 and len(dealer_hand) == 2:
+            print(f"Dealer WINS!  BlackJack!")
+            print(f"The Dealer's hand is : {dealer_hand}, which equals {sum(dealer_hand)}")
+            print(f"Your hand was {player_hand}, total score {sum(player_hand)}")
 
-    #if dealer doesnt have 21 but player does, player wins.
-    elif sum(dealer_hand) < 21 and sum(player_hand) == 21:
-        print(f"BlackJack! YOU WIN!")
-        print(f"The Dealers hand was {dealer_hand}")
-        print(f"Your hand was {player_hand}, total score {sum(player_hand)}")
-    else:
-        # ask the player if they want to hit and calculate the winner
-        print(f"The Dealers is showing: {dealer_hand[0]}.")
-        player_play(player=player_hand, dealer=dealer_hand)
-        dealer_play(player=player_hand, dealer=dealer_hand)
-        if sum(player_hand) > sum(dealer_hand) and sum(player_hand) < 22 and sum(dealer_hand) < 22:
-           print("Player wins")
-        elif sum(player_hand) < sum(dealer_hand) and sum(dealer_hand) < 22 and sum(player_hand) < 22:
-           print("Dealer Wins")
+        #if dealer doesnt have 21 but player does, player wins.
+        elif sum(dealer_hand) < 21 and sum(player_hand) == 21:
+            print(f"BlackJack! YOU WIN!")
+            print(f"The Dealers hand was {dealer_hand}")
+            print(f"Your hand was {player_hand}, total score {sum(player_hand)}")
         else:
-            print("It's a draw!")
-        print(f"Player's hand: {player_hand} and total: {sum(player_hand)}\n"
-              f"Dealer's hand: {dealer_hand} and total: {sum(dealer_hand)}")
+            # ask the player if they want to hit and calculate the winner
+            print(f"The Dealers is showing: {dealer_hand[0]}.")
+            player_play(player=player_hand, dealer=dealer_hand)
+            dealer_play(player=player_hand, dealer=dealer_hand)
+            if sum(player_hand) > sum(dealer_hand) and sum(player_hand) < 22 and sum(dealer_hand) < 22:
+               print("Player wins")
+            elif sum(player_hand) < sum(dealer_hand) and sum(dealer_hand) < 22 and sum(player_hand) < 22:
+               print("Dealer Wins")
+            else:
+                print("It's a draw!")
+            print(f"Player's hand: {player_hand} and total: {sum(player_hand)}\n"
+                  f"Dealer's hand: {dealer_hand} and total: {sum(dealer_hand)}")
+            play_again = input("Would you like to play again? 'y for yes, 'n' for no.").lower()
+            if play_again == 'y':
+                main()
+            else:
+                game_on = False
+
 
 
 def player_play(player, dealer):
@@ -68,7 +58,7 @@ def player_play(player, dealer):
                 print(f"You Busted with {(player)}  with a total of {sum(player)}\n"
                       f" Dealer Wins with {len(dealer)} cards and a total score of {sum(dealer)}!")
                 exit()
-                player_turn = False
+
             elif sum(player) > 21 and 11 in player:
                 for index in range(len(player)):
                     if player[index] == 11:
@@ -109,19 +99,12 @@ def dealer_play(player, dealer):
 
 
 
-
-
-
-
-
-
-
 cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 dealer_hand = []
 player_hand = []
 
 
 if __name__ == "__main__":
-    main()
+        main()
 
-    print("Thanks for Playing!")
+print("Thanks for Playing!")
