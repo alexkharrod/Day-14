f15a61b79153eb0e2902405814c21921de5e2cf0
alexharrod <alexkharrod@gmail.com>
2024-08-20 09:26:42 -0400

    Finished - random error with game not stopping - game_on variable doesnt effect according to pycharm

--- blackjack/main.py
+++ blackjack/main.py
@@ -5,70 +5,77 @@ import art
 
 def main():
     """ This is the main game loop that runs the game. deals original hand, and checks for blackjack"""
-    print(art.logo)
-    print("Welcome to BLACKJACK!")
-    dealer_hand = []
-    player_hand = []
+
     game_on = True
     while game_on:
+        print("Welcome to BLACKJACK!")
+        dealer_hand = []
+        player_hand = []
         for deal in range(2):
-            #deal the cards
+            # deal the cards
             dealer_hand.append(random.choice(cards))
             player_hand.append(random.choice(cards))
 
         # if dealer has black jack dealer wins
+        if sum(player_hand) == 22:
+            #  Just change the first one. handle_aces(player_hand)
+            player_hand[0] = 1
+
+        if sum(dealer_hand) == 22:
+            dealer_hand[0] = 1
+
         if sum(dealer_hand) == 21 and len(dealer_hand) == 2:
             print(f"Dealer WINS!  BlackJack!")
-            print(f"The Dealer's hand is : {dealer_hand}, which equals {sum(dealer_hand)}")
-            print(f"Your hand was {player_hand}, total score {sum(player_hand)}")
 
-        #if dealer doesnt have 21 but player does, player wins.
+
+        # if dealer doesn't have 21 but player does, player wins.
         elif sum(dealer_hand) < 21 and sum(player_hand) == 21:
             print(f"BlackJack! YOU WIN!")
-            print(f"The Dealers hand was {dealer_hand}")
-            print(f"Your hand was {player_hand}, total score {sum(player_hand)}")
+
+
         else:
             # ask the player if they want to hit and calculate the winner
             print(f"The Dealers is showing: {dealer_hand[0]}.")
             player_play(player=player_hand, dealer=dealer_hand)
-            dealer_play(player=player_hand, dealer=dealer_hand)
-            if sum(player_hand) > sum(dealer_hand) and sum(player_hand) < 22 and sum(dealer_hand) < 22:
-               print("Player wins")
-            elif sum(player_hand) < sum(dealer_hand) and sum(dealer_hand) < 22 and sum(player_hand) < 22:
-               print("Dealer Wins")
-            else:
-                print("It's a draw!")
-            print(f"Player's hand: {player_hand} and total: {sum(player_hand)}\n"
-                  f"Dealer's hand: {dealer_hand} and total: {sum(dealer_hand)}")
-            play_again = input("Would you like to play again? 'y for yes, 'n' for no.").lower()
-            if play_again == 'y':
-                main()
-            else:
-                game_on = False
+            if sum(player_hand) < 22:
+                dealer_play(player=player_hand, dealer=dealer_hand)
 
+            if sum(player_hand) < 22 and sum(dealer_hand) < 22:
+                if sum(player_hand) == sum(dealer_hand):
+                    print("It's a draw!")
+                elif sum(player_hand) > sum(dealer_hand):
+                    print("Player wins")
+                elif sum(player_hand) < sum(dealer_hand):
+                    print("Dealer Wins")
+
+        print(f"Player's hand: {player_hand} and total: {sum(player_hand)}\n"
+              f"Dealer's hand: {dealer_hand} and total: {sum(dealer_hand)}")
+
+        play_again = input("Would you like to play again? 'y for yes, 'n' for no.").lower()
+        if play_again == 'y':
+            print("\n" * 20)
+        else:
+            game_on = False
+            return
 
 
 def player_play(player, dealer):
     """ this function asks if you want to hit and shows your cards and the dealer showing card and calculates winner"""
     player_turn = True
     while player_turn:
-        hitme = input(f"Your cards are {player}, and your total is {sum(player)}\nDo you want to hit?  'y' for yes,"
-                    f" 'n' for no.\n").lower()
-        if hitme == 'y':
+        hit_me = input(f"Your cards are {player}, and your total is {sum(player)}\nDo you want to hit?  'y' for yes,"
+                       f" 'n' for no.\n").lower()
+        if hit_me == 'y':
             player.append(random.choice(cards))
             if sum(player) > 21 and 11 not in player:
-                print(f"You Busted with {(player)}  with a total of {sum(player)}\n"
-                      f" Dealer Wins with {len(dealer)} cards and a total score of {sum(dealer)}!")
+                print(f"You Busted, Dealer Wins!")
+                player_turn = False
                 return
 
             elif sum(player) > 21 and 11 in player:
-                for index in range(len(player)):
-                    if player[index] == 11:
-                        player[index] = 1
-                        print("Counting your Aces as 1 instead of 11!")
-             #need to end game as well
+                handle_aces(player)
 
-        elif hitme == 'n':
+        elif hit_me == 'n':
             player_turn = False
         # make the game end if you BUST
 
@@ -77,36 +84,24 @@ def dealer_play(player, dealer):
     while sum(dealer) < 17:
         dealer.append(random.choice(cards))
         if sum(dealer) > 21 and 11 not in dealer:
-            print(f"Dealer Busted with {dealer}  with a total of {sum(dealer)}\n"
-                  f" You  Win with {len(player)} cards and a total score of {sum(player)}!")
-
+            print(f"Dealer Busted, You Win!")
             return
         elif sum(dealer) > 21 and 11 in dealer:
-            for index in range(len(dealer)):
-                if dealer[index] == 11:
-                    dealer[index] = 1
-                    print("Counting Dealers Aces as 1 instead of 11!")
-        else:
-            dealer_turn = False
+            handle_aces(dealer)
 
 
+def handle_aces(whose_hand):
+    for index in range(len(whose_hand)):
+        if whose_hand[index] == 11:
+            whose_hand[index] = 1
+            print(f"Counting {whose_hand} Aces as 1 instead of 11!")
 
 
+# cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+cards = [10, 11, 10, 11, 10, 11]
 
-
-        #end the game if dealer busts
-
-
-
-
-
-
-cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-dealer_hand = []
-player_hand = []
-
-
+print(art.logo)
 if __name__ == "__main__":
-        main()
+    main()
 
 print("Thanks for Playing!")
